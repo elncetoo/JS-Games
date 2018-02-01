685a99c3efd09e7d59a285183f402f12222262a2
elincetoo <svilena_koleva@yahoo.com>
2018-02-01 12:09:09 +0100

    function ext

---
+++ basic.js
@@ -0,0 +1,72 @@
+// JavaScript Document
+//global varaiables
+	var number1 = 1;
+	var number2 = 11;
+	var output = document.getElementById("output");
+	var trigger = document.getElementById("trigger");
+
+
+	//regular function with a return value
+	function add() {
+		'use strict';
+		//local var
+		var result = number1 + number2;
+		return result;
+
+	}
+	console.log(add()); //returns the value
+
+	//ECMA 6 arrow function, with multiple statements u need to include the function body {}
+	var result = () => number1 + number2;//=> is replacing the whole function content
+	console.log(result()); //because var result is now serving as a function
+
+
+	//function with user input
+	function userInput() {
+		'use strict';
+	//let is restricting the scope to the acatual code statemnt code block statement or expression
+		let number1 = parseInt(prompt('Type a number:', ''));//to capture user input-call the prompt method(srgiments-text displayed,epmpy space foruser input)
+		let number2 = parseInt(prompt('Type a number:', ''));
+		let result = number1 + number2;
+		
+		//rendering the result
+
+		output.innerHTML = 'The result is: ' + result + '!';
+	}
+
+	//userInput();
+	//when we get null means object does not exist
+
+//constructor function
+function player(name, score){
+	'use strict';
+	this.name = name;
+	this.score = score;
+
+	this.greeting = function(){
+		alert("Hi "+this.name+' Your score is '+ this.score);
+	}
+}
+
+var myPlayer = new player ('Peter', '55');
+myPlayer.greeting();
+
+//creating a n dimentional array using litteral notation
+var playerScore = [
+	['Jessica', 0],
+	['Simone', 7],
+	['Vladi', 9],
+	['Ida', 10]
+	
+];
+
+playerScore.push(['Peter',55]);
+
+function listPlayers(){
+	'use strict';
+	for(var i=0; i<playerScore.length; i++){
+		output.innerHTML += playerScore[i] + '<br>';
+	}
+}
+
+trigger.addEventListener('click', listPlayers);
