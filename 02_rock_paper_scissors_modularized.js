3971a6b7d1ab75174bf6ac4eb36976948e83a63f
elincetoo <svilena_koleva@yahoo.com>
2018-02-08 11:21:18 +0100

    Rock-Paper-Scissors-Game
    programming game logic

--- 02_rock_paper_scissors_modularized.js
+++ 02_rock_paper_scissors_modularized.js
@@ -14,8 +14,55 @@ var userChoice;
 
 // main function containing the game logic
 function play(){
-	userChoice = this.id; // registering the buttons id!
+	//this in a js function context (!) can reffer to the current owner of the event
+	userChoice = this.id; // registering the buttons id!//the btn that is triggering the func become this.id0.
 	computerInput();
 	compare();
 	display();	
 }
+
+function computerInput(){
+	'use strict';
+
+	switch(Math.floor(Math.random()*3)){      //returns a whole number between 0-2
+		
+		case 0:
+			computerChoice = "Rock";
+			break; //code stopsexecuting when right
+		case 1:
+			computerChoice = "Paper";
+			break;
+		case 2:
+			computerChoice = "Scissors";
+		
+		}
+			console.log(computerChoice);
+	}
+	
+function compare(){
+	
+	//game is  a draw
+	if(userChoice === computerChoice){
+		resultDisplay.innerHTML = 'Game is a draw biAtch, pleaseeee';
+		//all posibilities
+		} else if(userChoice === 'Paper' && computerChoice = 'Rock'){
+			resultDisplay.innerHTML = 'You win!!!';
+		} else if(userChoice === 'Paper' && computerChoice = 'Scissors'){
+			resultDisplay.innerHTML = 'You loose!!!';
+		} else if(userChoice === 'Scissors' && computerChoice = 'Rock'){
+				resultDisplay.innerHTML = 'You loose!!!';
+		} else if(userChoice === 'Scissors' && computerChoice = 'Paper'){
+				resultDisplay.innerHTML = 'You win!!!';
+		} else if(userChoice === 'Rock' && computerChoice = 'Scissors'){
+				resultDisplay.innerHTML = 'You win!!!';
+		} else (userChoice === 'Rock' && computerChoice = 'Paper'){
+				resultDisplay.innerHTML = 'You loose!!!';
+	}	
+	
+function display(){
+	//to make user and computer choise visible
+	playerChoiceDisplay.innerHTML = userChoice;
+	computerChoiceDisplay.innerHTML = computerChoice;
+	
+	}	
+	
